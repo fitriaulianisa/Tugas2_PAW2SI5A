ad5627e2a6cd68cdf53bc56f5a6ce9e968705dd4
stepheniew <stephenlim2004@gmail.com>
2024-10-08 15:40:24 +0700

    update

--- app-restaurant/routes/index.js
+++ app-restaurant/routes/index.js
@@ -6,35 +6,18 @@ router.get('/', function(req, res, next) {
   res.render("home", { title: 'Home', layout:'main' });
 });
 router.get('/pegawai', function(req, res, next) {
-  const pegawai = [
-    {
-        nama: "Chef Juna",
-        role: "Chef",
-       
-    },
-    {
-        nama: "Chef Renata",
-        role: "Chef",
-    },
-    {
-        nama: "Chef Spongebob",
-        role: "Chef",
-    },
-    {
-        nama: "Karina",
-        role: "Kasir",
-    },
-    {
-        nama: "Joko",
-        role: "Pramusaji",
-    },
-    {
-        nama: "Adit",
-        role: "Pramusaji",
-    },
-
-];
-  res.render("pegawai", { title: 'Pegawai',pegawai, layout:'main' });
+  res.json({
+    "message" : "Daftar Pegawai",
+    "data": [{nama:"Juna", role:"Chef"}, 
+             {nama:"Renata", role:"Chef"},
+             {nama:"Spongebob", role:"Chef"},
+             {nama:"Karina", role:"Kasir"},
+             {nama:"Joko", role:"Pramusaji"},
+             {nama:"Adit", role:"Pramusaji"},
+          
+            ]
+          });
+  
 });
 router.get('/about', function(req, res, next) {
   res.render("about", { title: 'About', layout:'main' });
