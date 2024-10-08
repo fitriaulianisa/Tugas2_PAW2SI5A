760a46320656bc6ace47f05b9e225a2f83025383
stepheniew <stephenlim2004@gmail.com>
2024-10-08 13:08:34 +0700

    update

--- app-restaurant/routes/index.js
+++ app-restaurant/routes/index.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render("home", { title: 'Home', layout:'main' });
 });
+router.get('/pegawai', function(req, res, next) {
+  res.render("pegawai", { title: 'Pegawai', layout:'main' });
+});
+router.get('/about', function(req, res, next) {
+  res.render("about", { title: 'About', layout:'main' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render("contact", { title: 'Contact', layout:'main' });
+});
+router.get('/menu', function(req, res, next) {
+  res.render("menu", { title: 'Menu', layout:'main' });
+});
 
 module.exports = router;
